115ff97ebcabbf283bbe25bb358f40c50d0d3212
Light-lig <lightcaceres@gmail.com>
2021-05-11 18:36:28 -0600

    Mejoramiento de apis

--- src/main/java/com/sistemaMotelario/core/dao/MotelDao.java
+++ src/main/java/com/sistemaMotelario/core/dao/MotelDao.java
@@ -2,7 +2,6 @@ package com.sistemaMotelario.core.dao;
 
 import java.util.List;
 
-import com.sistemaMotelario.core.entity.SmFotos;
 import com.sistemaMotelario.core.entity.SmHabitacion;
 import com.sistemaMotelario.core.entity.SmMotel;
 
@@ -11,7 +10,7 @@ public interface MotelDao {
 	public abstract List<SmMotel> findAll();
 	public abstract List<SmMotel> findByMunIdandCatId(int idmunicipio, int idcategoria, String nombre);
 	public abstract SmMotel findByMoId(int moId);
-	public abstract List<SmFotos> findHabitacion(int moId);
+	public abstract List<SmHabitacion> findHabitacion(int moId);
 	public abstract SmHabitacion findById(int idHabitacion);
 
 }
